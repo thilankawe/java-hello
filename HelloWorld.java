714b9f2c8b4a632f6dbab06ec9fe579aa831be28
thilankawe <108317482+thilankawe@users.noreply.github.com>
2023-01-24 13:05:31 +0530

    Update HelloWorld.java
    Add try 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World changed after hook");
+    System.out.println("Hello, World changed after hook try 2");
   }
 }
